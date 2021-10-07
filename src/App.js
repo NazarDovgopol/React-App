3e36f0580db8e44ae7a24d79cfb6c5915260d8a4
Nazar Dovgopol <nazar.dovgopol.nazar@gmail.com>
2021-10-07 12:21:43 +0300

    test react 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import Car from './Car/Car';
+import React, { useState } from 'react';
 
 function App() {
+  const [cars, setCars] = useState(
+    [
+      {name: 'Ford', year: 2018},
+      {name: 'Audi', year: 2016},
+      {name: 'Mazda', year: 2010},
+    ]
+  );
+
+  const [pageTitle] = useState(
+    'Hello React component!'
+  );
+
+  const [showCars, setShowCars] = useState(
+    false
+  );
+
+  const toggleCarsHandler = () => {
+    setShowCars(
+      !showCars
+    );
+  }
+
+  const onChangeName = (name, index) => {
+    const car = cars[index]
+    const allCars = [...cars]
+
+    car.name = name
+    allCars[index] = car
+
+    setCars(
+      allCars
+    );
+  }
+
+  const deleteHandler = index => {
+    const allCars = [...cars]
+    allCars.splice(index, 1)
+
+    setCars(
+      allCars
+    )
+  }
+
+  let listCars = null;
+
+  if (showCars) {
+    listCars = cars.map((car, index) => {
+
+      return (
+        <Car
+          key = {index}
+          name = {car.name}
+          year = {car.year}
+          onDelete = {() => deleteHandler(index)}
+          onChangeName = {({target}) => onChangeName(target.value, index)}
+        />
+      )
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1>{pageTitle}</h1>
+
+      <button onClick = {toggleCarsHandler}>Toggle cars</button>
+
+      <div style={{
+        width: 400,
+        margin: 'auto',
+        paddingTop: '20px'
+      }}>
+        {listCars}
+      </div>
     </div>
   );
 }
